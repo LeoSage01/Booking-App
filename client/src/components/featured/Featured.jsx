6d967d7bc3eba9f572299c8760c11dc09cf79c90
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-06-30 07:10:22 +0100

    Add files via upload

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -2,8 +2,9 @@ import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
-
-  const { data, loading, error } = useFetch("http://localhost:8000/api/hotels/countByCity?cities=berlin,florida,london")
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrid,london"
+  );
 
   return (
     <div className="featured">
@@ -30,7 +31,7 @@ const Featured = () => {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Florida</h1>
+              <h1>Madrid</h1>
               <h2>{data[1]} properties</h2>
             </div>
           </div>
